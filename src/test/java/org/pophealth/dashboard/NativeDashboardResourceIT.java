f5a388142faff7aa151800ab5abb5bfe28496846
joewhite101 <j.white@entando.com>
2020-07-12 16:52:46 +0200

    Add setup

---
+++ src/test/java/org/pophealth/dashboard/NativeDashboardResourceIT.java
@@ -0,0 +1,9 @@
+package org.pophealth.dashboard;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeDashboardResourceIT extends DashboardResourceTest {
+
+    // Execute the same tests but in native mode.
+}
